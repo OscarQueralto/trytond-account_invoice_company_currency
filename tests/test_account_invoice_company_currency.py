de11c7126b81ab6195760387c6f8cd538d70bdca
Sergi Almacellas Abellana <sergi@koolpi.com>
2015-05-08 16:29:08 +0200

    Add tests scenario

--- tests/test_account_invoice_company_currency.py
+++ tests/test_account_invoice_company_currency.py
@@ -1,15 +1,18 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
+import doctest
 import unittest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import doctest_setup, doctest_teardown
 
 
 class TestCase(unittest.TestCase):
     'Test module'
 
     def setUp(self):
-        trytond.tests.test_tryton.install_module('account_invoice_company_currency')
+        trytond.tests.test_tryton.install_module(
+            'account_invoice_company_currency')
 
     def test0005views(self):
         'Test views'
@@ -23,4 +26,8 @@ class TestCase(unittest.TestCase):
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
+    suite.addTests(doctest.DocFileSuite(
+            'scenario_invoice_company_currency.rst',
+            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
