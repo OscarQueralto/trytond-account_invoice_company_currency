d7804f734f52822e230fb1c4b891f8577ed37441
Jes?s Mart?n Jim?nez <jesus@nan-tic.com>
2016-07-01 10:55:39 +0200

    Fix test

--- tests/test_account_invoice_company_currency.py
+++ tests/test_account_invoice_company_currency.py
@@ -3,24 +3,13 @@
 import doctest
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_setup, doctest_teardown
 
 
-class TestCase(unittest.TestCase):
+class TestCase(ModuleTestCase):
     'Test module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module(
-            'account_invoice_company_currency')
-
-    def test0005views(self):
-        'Test views'
-        test_view('account_invoice_company_currency')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'account_invoice_company_currency'
 
 
 def suite():
