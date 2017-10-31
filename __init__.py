6b72d6077bcd236844bc5a2d547313bef89a88cc
Albert Cervera i Areny <albert@nan-tic.com>
2017-10-31 18:48:31 +0100

    Several fixes and improvements:
    - PEP8 fixes to __init__.py
    - Remove unused cache field on account.invoice.tax
    - Add missing company_currency_digits field in invoice line and add depends to
      company_amount
    - Improve tests to check invoice line and tax amounts

--- __init__.py
+++ __init__.py
@@ -1,12 +1,11 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from .invoice import *
-
+import invoice
 
 def register():
     Pool.register(
-        Invoice,
-        InvoiceTax,
-        InvoiceLine,
+        invoice.Invoice,
+        invoice.InvoiceTax,
+        invoice.InvoiceLine,
         module='account_invoice_company_currency', type_='model')
